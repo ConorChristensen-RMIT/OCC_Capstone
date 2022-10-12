1bfcd487bfce6b7126739bead4c79392f0dcd9f5
ConorChristensen-RMIT <s3815282@student.rmit.edu.au>
2022-10-12 19:47:27 +1100

    Decoding packet

--- Arduino/Blink/Blink.ino
+++ Arduino/Blink/Blink.ino
@@ -21,22 +21,45 @@
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
-double delay_ms;
+
+int preamble[9] = {1, 1, 1, 1, 1, 1, 1, 0, 0};
+int packet[8] = {0, 0, 1, 0, 0, 1, 0, 0};
+int parity[2] = {0, 0};
+int loop_counter = -1;
+
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(2, OUTPUT);
-  int camera_h = 720;
-  int camera_hz = 30;
-  double period_ms = 1000/(camera_hz*camera_h);
-  delay_ms = period_ms/2;
+//  int camera_h = 720;
+//  int camera_hz = 30;
+//  double period_ms = 1000/(camera_hz*camera_h);
+//  delay_ms = period_ms/2;
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  // TO KEEP 1 THEN 0 FOR 2 SEPERATE PERIODS
-  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(delay_ms);                       // wait for a second
-  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
-  delay(delay_ms);                       // wait for a second
+  loop_counter++;
+  if (loop_counter < 9) {
+    if (preamble[loop_counter] == 0) {digitalWrite(2, LOW);}
+    else {digitalWrite(2, HIGH);}
+  }
+  else if (loop_counter < 17) {
+    if (packet[loop_counter-9] == 0) {digitalWrite(2, LOW);}
+    else {digitalWrite(2, HIGH);}
+  }
+  else if (loop_counter < 19) {
+    if (parity[loop_counter-17] == 0) {digitalWrite(2, LOW);}
+    else {digitalWrite(2, HIGH);}
+  }
+  else {loop_counter = -1;}
+  
+  delayMicroseconds(350);
+//  // TO KEEP 1 THEN 0 FOR 2 SEPERATE PERIODS
+//  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delayMicroseconds(300);                       // Should be 1929, but for some reason 1926 appears best
+//  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+//  delayMicroseconds(300);                       // Should be 1929, but for some reason 1926 appears best
 }
--- TestingCV2/testingcv2.py
+++ TestingCV2/testingcv2.py
@@ -1,7 +1,8 @@
 import cv2
 import time
 import numpy as np
-
+from statistics import mean
+import copy
 
 class LightReader:
     def __init__(self, roi_x, roi_y, roi_w, roi_h):
@@ -22,11 +23,11 @@ def getROIImage(img, roi_x, roi_y, roi_w, roi_h):
     return crop_img
 
 
+# Read the pixels from a binary image utilising Rolling Shutter Principles
 def readBinPixels(img, img_t_h, ybuff, img_Hz, tx_Hz, p_len):
     T_tx = 1 / tx_Hz
     T_t = 1 / img_Hz
     T_row = T_t / img_t_h
-
     img_h = img.shape[0]
     if img_h - 2 * ybuff * (T_tx / T_row) < p_len:
         raise Exception("Insufficient pixel-height")
@@ -44,26 +45,52 @@ def readBinPixels(img, img_t_h, ybuff, img_Hz, tx_Hz, p_len):
                 output.append(0)
     return output
 
+
+# Incomplete function for reading data. Need to finalise encoding first.
 def readData(out_arr):
     preamble = np.array([1, 1, 1, 1, 1, 1, 0])
     np_arr = np.array(out_arr)
-    #for i in range(len(np_arr)):
+    # for i in range(len(np_arr)):
 
 
+def getRowsOutput(roi, thresh):
+    outArr = []
+    for ind, row in enumerate(roi):
+        if mean(row) >= thresh:
+            roi[ind] = np.multiply(np.ones(len(row)), 255)
+            outArr.append(1)
+        else:
+            roi[ind] = np.zeros(len(row))
+            outArr.append(0)
+    return outArr
+
+
+def processRowsOutput(rowArr):
+    outArr = []
+    counter = 0
+    for i in range(1, len(rowArr)):
+        if rowArr[i-1] == rowArr[i]:
+            counter += 1
+        else:
+            outArr.append((counter, rowArr[i-1]))
+            counter=0
+    outArr.append((counter, rowArr[i]))
+    return outArr
 
 
 def ReadLight():
     # All of these can be set using a detection library
-    roi_x = 100
-    roi_y = 100
-    roi_w = 150
-    roi_h = 150
+    roi_x = 490
+    roi_y = 360
+    roi_w = 350
+    roi_h = 350
 
-    #cap = cv2.VideoCapture(1)  # Select web cam 0
+    # cap = cv2.VideoCapture(1)  # Select web cam 0
     fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
     cap = cv2.VideoCapture()
-    cap.open(0 + 1 + cv2.CAP_DSHOW)
+    cap.open(0, cv2.CAP_DSHOW)
     cap.set(cv2.CAP_PROP_FOURCC, fourcc)
+
     cam_w_4k = 3840
     cam_h_4k = 2160
     cam_w_1080 = 1920
@@ -71,13 +98,18 @@ def ReadLight():
     frame_rate_1080 = 30
     frame_rate_4k = 30
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, cam_w_1080)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_h_1080)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_w_1080)
     cap.set(cv2.CAP_PROP_FPS, frame_rate_1080)
-
-    #change on change of cam dims
+    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1) # Set to 3 for auto and 1 for manual
+    cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+    cap.set(cv2.CAP_PROP_EXPOSURE, -11) # Minimum Allowable Exposure
+    cap.set(cv2.CAP_PROP_FOCUS, 255)
+    # print('Exposure: ', cap.get(cv2.CAP_PROP_EXPOSURE))
+    print("FR: ", cap.get(cv2.CAP_PROP_FPS))
+    print("Focus", cap.get(cv2.CAP_PROP_FOCUS))
+    # change on change of cam dims
     cam_h = cam_h_1080
     cam_w = cam_w_1080
-
 
     # cap.set(3, cam_w)
     # cap.set(4, cam_h)
@@ -85,41 +117,55 @@ def ReadLight():
 
     light_reader = LightReader(roi_x, roi_y, roi_w, roi_h)
     hz = round(cap.get(cv2.CAP_PROP_FPS))
+    init_time = time.time()
+    current_time = time.time()
+    out_str = ""
+    proc_out_str = ""
 
-    while True:
+    linesOnScreen = 19
+    lineStartCords = []
+    lineEndCords = []
+    for h in range(1, linesOnScreen):
+        lineStartCords.append((1, h*(roi_h // linesOnScreen)))
+        lineEndCords.append((249, h*(roi_h // linesOnScreen)))
+
+    while current_time-init_time < 10:
         # Time the loop
         current_time = time.time()
         p_hz = 1 / (current_time - previous_time)
-        if p_hz > 30:
-            continue
+        # if p_hz > frame_rate_1080 + 1:
+        #     continue
         previous_time = current_time
-
 
         # Read image and process
         success, img_r = cap.read()
-        img_g = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
-        roi_i = getROIImage(img_g, roi_x, roi_y, roi_w, roi_h)
-        img = light_reader.draw_region(img_g)
-        th, roi_bin = cv2.threshold(roi_i, 128, 255, cv2.THRESH_OTSU)
+        img = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
+        roi = getROIImage(img, roi_x, roi_y, roi_w, roi_h)
 
-        # Packet length Preamble+Payload+Parity = 7+32+1 = 40
-        p_len = 40
-        # The sample rate of rows in camera
-        tx_Hz = hz * cam_h
+        roi = cv2.equalizeHist(roi)
+        # roi_i_eq = copy.deepcopy(roi_i)
+        # print(np.amax(roi_i))
+        img = light_reader.draw_region(img)
 
         # Read the data
-        ybuff = 3
-        out_arr = readBinPixels(roi_bin, cam_h, ybuff, hz, tx_Hz, p_len)
-        print(out_arr)
+        outArr = getRowsOutput(roi, 80)
+        out_str += str(outArr) + "\n"
 
-
-
-        # Display the image(s)
-        cv2.putText(img, "Cam HZ: " + str(int(hz)), (15, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+        proc_out_str += str(processRowsOutput(outArr)) + "\n"
+        cv2.putText(img, "Cam HZ Setting: " + str(int(hz)), (15, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
         cv2.putText(img, "Loop HZ: " + str(p_hz), (15, 100), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
-        cv2.imshow("roi", roi_bin)
+        # for j in range(linesOnScreen-1):
+        #     cv2.line(roi_bin, lineStartCords[j], lineEndCords[j], (255, 0, 0), 2)
+        cv2.imshow("roi", roi)
+        # cv2.imshow("roi_eq", roi_i_eq)
         cv2.imshow("image", img)
         cv2.waitKey(1)
+
+    with open('out_at_350us_T.txt', 'w') as f:
+        f.write(out_str)
+
+    with open('Proc_out_at_350us_T.txt', 'w') as f:
+        f.write(proc_out_str)
 
 
 if __name__ == '__main__':
